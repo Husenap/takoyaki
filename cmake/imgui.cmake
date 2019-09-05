01bcede6a75dc6609c339ff10749302ccf1b86f6
Hussein Taher <Husenap@users.noreply.github.com>
2019-09-06 00:08:14 +0200

    Created basic demo window with imgui/glad initialized (#1)
    * created basic window with glad initialised
    
    * fixed yml file
    
    * lowered cmake versin
    
    * updated build script
    
    * removed spaced
    
    * build only windows
    
    * added imgui

--- cmake/imgui.cmake
+++
@@ -1,9 +0,0 @@
-include(FetchContent)
-
-FetchContent_Declare(
-    imgui
-    GIT_REPOSITORY  https://github.com/ocornut/imgui.git
-    GIT_TAG         v1.72b
-)
-
-FetchContent_MakeAvailable(imgui)
--- takoyaki/src/main.cpp
+++ takoyaki/src/main.cpp
@@ -1,9 +1,74 @@
 #include <iostream>
 
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+static void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	}
+}
+
 int main() {
-	std::cout << "Hello world" << std::endl;
+	if (!glfwInit()) {
+		return 1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "TakoYaki", NULL, NULL);
+	if (!window) {
+		return 1;
+	}
+
+	glfwSetKeyCallback(window, KeyCallback);
+
+	glfwMakeContextCurrent(window);
+	gladLoadGL();
+	glfwSwapInterval(1);
+
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 450");
+
+	bool showDemoWindow = true;
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		if (showDemoWindow) ImGui::ShowDemoWindow(&showDemoWindow);
+
+		ImGui::Render();
+
+
+		int width, height;
+		glfwGetFramebufferSize(window, &width, &height);
+		glViewport(0, 0, width, height);
+		glClearColor(0.18f, 0.18f, 0.18f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		glfwSwapBuffers(window);
+	}
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 
 	return 0;
 }
