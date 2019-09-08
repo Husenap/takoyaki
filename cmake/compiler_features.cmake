e90f02a2f1106a8c0f7a15ae7fe73b2d49ba02c1
Hussein Taher <hussein_taher@outlook.com>
2019-09-08 18:40:35 +0200

    how about now?!

--- cmake/compiler_features.cmake
+++ cmake/compiler_features.cmake
@@ -3,7 +3,7 @@ if (MSVC)
     # In VS2017 15.9.3 this is needed to correctly identify nested namespace definitions
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /std:c++17")
 else()
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++1z")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++1z -lc++experimental")
 endif()
 
 message("-- CMAKE_CXX_FLAGS: ${CMAKE_CXX_FLAGS}")
