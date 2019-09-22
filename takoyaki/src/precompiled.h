c402bcbb9939252bb2bbfa79383ef0f1582d21fe
Hussein Taher <hussein_taher@outlook.com>
2019-09-22 19:42:07 +0200

    added docking

--- takoyaki/src/precompiled.h
+++ takoyaki/src/precompiled.h
@@ -22,8 +22,8 @@
 #include "util/glmvecToImVec.h"
 
 #include <imgui.h>
-#include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
+#include <examples/imgui_impl_glfw.h>
+#include <examples/imgui_impl_opengl3.h>
 
 #include <glad/glad.h>
 
